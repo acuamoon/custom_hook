88da17ba6ab082d68a0ef17131b1fdfa0e16723d
kangmoonseok87 <acuamoon1@gmail.com>
2023-10-31 14:47:32 +0900

    Creat React Project

---
+++ src/App_test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Component from './components/Component';
+
+
+function App_test() {
+
+   return (
+    <div className='App_test'>
+      <Component />
+    </div>
+  )
+}
+
+export default App_test;
